f41528bd71897bed99591e07ce4c19a209e0693d
foughali-wejdene <61500354+foughali-wejdene@users.noreply.github.com>
2023-03-27 11:39:17 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mspr/Feature/Login%20Screen/Login_Screen.dart';
 
 import 'package:mspr/main.dart';
-import 'dart:core';
 
 void main() {
-  testWidgets('Test de lancement de l\'application', (WidgetTester tester) async {
-    // Construire l'application
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Vérifier que l'écran de connexion est affiché
-    expect(find.byType(LoginScreen), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
