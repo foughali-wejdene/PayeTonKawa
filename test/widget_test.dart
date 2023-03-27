30408e4be2be36c4fbe8fc3da788e7ce97909060
foughali-wejdene <61500354+foughali-wejdene@users.noreply.github.com>
2023-03-27 12:03:00 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mspr/Feature/Login%20Screen/Login_Screen.dart';
 
 import 'package:mspr/main.dart';
+import 'package:mspr/widget/background.dart';
 import 'dart:core';
 
 import 'package:mspr/widget/product_bloc.dart';
@@ -21,4 +22,13 @@ test('productItems stream should emit list of products', () async {
     // Expect that the list contains at least one product
     expect(list.length, greaterThan(0));
   });
+
+  test('ArcClipper should return a path with correct shape', () {
+    final clipper = ArcClipper();
+    final path = clipper.getClip(Size(200, 200));
+    expect(path.contains(Offset(100, 190)), isTrue); // check if the curve is centered
+    expect(path.contains(Offset(0, 170)), isTrue); // check if the left edge is correct
+    expect(path.contains(Offset(200, 170)), isTrue); // check if the right edge is correct
+  });
+  
 }
